eb54be299791623905d0309bcdfdb821133ddfe0
Felipe Marinho <felipevm97@gmail.com>
2019-01-01 18:59:03 +0000

    add scroll endpoints and now filling videos with numbers

--- main.js
+++ main.js
@@ -24,25 +24,29 @@ app.get('/', (req, res) => {
 })
 
 app.get('/controller.js', (req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/javascript'});
-    console.log(controller);
-    
+    res.writeHead(200, {'Content-Type': 'text/javascript'});    
     res.end(controller.replace(/localhost/g, ip));
 })
 
 app.get('/pesquisar', (req, res) => {
-    driver.get(`https://www.youtube.com/results?search_query=${req.param('query')}`)
+    driver.get(`https://www.youtube.com/results?search_query=${req.param('query')}&sp=EgIQAQ%253D%253D`)
+
+    fillResultsVideosWithNumbers(3000)
 
     return res.send({ status: 'OK' })
 })
 
 app.get('/escolher', (req, res) => {
-    
-    element = driver.findElement(
-        By.xpath(
-            `/html[1]/body[1]/ytd-app[1]/div[1]/ytd-page-manager[1]/ytd-search[1]/div[1]/ytd-two-column-search-results-renderer[1]/div[1]/ytd-section-list-renderer[1]/div[2]/ytd-item-section-renderer[1]/div[2]/ytd-video-renderer[${req.param('num')}]/div[1]`))
-        
+    const videoSuggestions= `/html[1]/body[1]/ytd-app[1]/div[1]/ytd-page-manager[1]/ytd-watch-flexy[1]/div[3]/div[2]/div[1]/div[2]/ytd-watch-next-secondary-results-renderer[1]/div[2]/ytd-compact-video-renderer[${req.param('num')}]/div[1]/a[1]/h3[1]/span[1]`
+    const searchResults = `/html[1]/body[1]/ytd-app[1]/div[1]/ytd-page-manager[1]/ytd-search[1]/div[1]/ytd-two-column-search-results-renderer[1]/div[1]/ytd-section-list-renderer[1]/div[2]/ytd-item-section-renderer[1]/div[2]/ytd-video-renderer[${req.param('num')}]/div[1]`;
+
+    element = driver.findElement(By.xpath(searchResults))
     element.click()
+
+    element = driver.findElement(By.xpath(videoSuggestions))
+    element.click()
+
+    fillSugestionVideosWithNumbers(3000);
 
     return res.send({ status: 'OK' })
 })
@@ -54,7 +58,6 @@ app.get('/tela-cheia', (req, res) => {
             By.xpath('//iframe[starts-with(@src, "https://www.youtube.com/embed")]')))
             .catch((err) => console.log("erro"))
     } catch (e) {
-
     }
 
     driver.findElement(By.xpath(`//button[@class='ytp-fullscreen-button ytp-button']`))
@@ -137,5 +140,38 @@ app.get('/proximo', (req, res) => {
     return res.send({ status: 'OK' })
 })
 
+app.get('/scroll', (req, res) => {
+    let direction = req.param('direction') === 'up' ? '-' : '+';
+    
+    driver.executeScript(`window.scrollBy(0,${direction}150)`)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+    return res.send({ status: 'OK' })
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!\nRunning on network ${ip}`))
+function fillSugestionVideosWithNumbers(timeout) {
+    driver.sleep(timeout).then(() => {
+        for (let i = 0; i < 11; i++) {
+            const indicator = `<span style="background: #ff5959;width: 30px;height: 24px;position: absolute;border-radius: 1em;text-align: center;color: white;font-weight: bold;left: 5px;top: 5px;padding-top: 6px;">${i}</span>`
+            const videoSuggestion = `/html[1]/body[1]/ytd-app[1]/div[1]/ytd-page-manager[1]/ytd-watch-flexy[1]/div[3]/div[2]/div[1]/div[2]/ytd-watch-next-secondary-results-renderer[1]/div[2]/ytd-compact-video-renderer[${i}]/div[1]/a[1]/h3[1]/span[1]`;
+            let timeElement = driver.findElement(By.xpath(videoSuggestion));
+            timeElement.getAttribute('title').then((value) => {
+                driver.executeScript(`arguments[0].innerHTML = '${indicator + value}';`, timeElement);
+            });
+        }
+    });
+}
+
+function fillResultsVideosWithNumbers(timeout) {
+    driver.sleep(timeout).then(() => {
+        for (let i = 0; i < 11; i++) {
+            const indicator = `<span style="background: #ff5959;width: 30px;height: 24px;position: absolute;border-radius: 1em;text-align: center;color: white;font-weight: bold;left: 5px;top: 5px;padding-top: 6px;">${i}</span>`
+            const resultVideo = `/html[1]/body[1]/ytd-app[1]/div[1]/ytd-page-manager[1]/ytd-search[1]/div[1]/ytd-two-column-search-results-renderer[1]/div[1]/ytd-section-list-renderer[1]/div[2]/ytd-item-section-renderer[1]/div[2]/ytd-video-renderer[${i}]/div[1]/div[1]/div[1]/div[1]/h3[1]/a[1]`;
+            let timeElement = driver.findElement(By.xpath(resultVideo));
+            timeElement.getAttribute('title').then((value) => {
+                driver.executeScript(`arguments[0].innerHTML = '${indicator + value}';`, timeElement);
+            });
+        }
+    });
+}
